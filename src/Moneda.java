956b36110ba2d322193e9fd9a253cf9f3ad75f80
Cholo0022 <andres.bozzani@gmail.com>
2022-05-24 13:59:11 -0300

    Clase padre de moneda creada

---
+++ src/Moneda.java
@@ -0,0 +1,26 @@
+
+public class Moneda {
+	private String nombre;
+	private String valor;
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public Moneda() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
